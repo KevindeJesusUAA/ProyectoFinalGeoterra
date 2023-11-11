61b4f089f4d312639ae79ff69fc48da40231b825
KevindeJesusUAA <118912199+KevindeJesusUAA@users.noreply.github.com>
2023-11-11 15:26:00 -0600

    Agregué el mapa para explorar las maravillas del mundo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -44,4 +45,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
 }
--- app/src/main/java/uaa/mx/proyectofinalgeoterra/explora.java
+++ app/src/main/java/uaa/mx/proyectofinalgeoterra/explora.java
@@ -1,16 +1,48 @@
 package uaa.mx.proyectofinalgeoterra;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class explora extends AppCompatActivity {
+public class explora extends AppCompatActivity implements OnMapReadyCallback {
 
-    //En el método onCreate creamos la actividad y se configur
+    //En el método onCreate creamos la actividad y se configura
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); // Esta línea llama al método onCreate de la clase base (AppCompatActivity) para que realice sus tareas de inicialización
-        setContentView(R.layout.explora_conmigo); // Se carga la interfaz de usuario de la actividad a partir del archivo de diseño para que los distintos elementos
-        // estén disponibles
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.explora_conmigo);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     *
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(21.91415, -102.3168);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
